b8a096ae8595963e742111bd0669e3eccc28f657
egemen-dev <egemenwasd@gmail.com>
2023-06-05 23:20:16 +0300

    Create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="mt-24 footer footer-center p-8 bg-base-200 text-base-content rounded">
+      <div>
+        <p>Thanks for visiting! </p>
+        <br />
+        <p>Egemen Öztürk - 2023</p>
+      </div>
+    </footer>
+  );
+}
