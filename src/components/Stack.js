cf190020095f6f3598add5ea8ec0012bd7d3864c
egemen-dev <egemenwasd@gmail.com>
2023-06-06 16:43:11 +0300

    Refactor stack component

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -4,39 +4,19 @@ import Linkedin from "./icons/Linkedin";
 
 export default function Stack() {
   return (
-    <div id="stack-container" className="stack px-6 reveal-after-3-seconds">
-      <div
-        id="stack-0"
-        className="card text-center bg-gray-50 dark:bg-gray-700 card bg-base-100 mb-10 border-2 border-gray-300 dark:border-gray-800"
-      >
-        <div className="card-body items-center">
-          <h2 className="card-title dark:text-gray-100">Connect with me now!</h2>
-          {/*add arrow down */}
+    <div className="card-body items-center shadow-xl bg-base-100 rounded-box dark:bg-base-200">
+      <h2 className="card-title dark:text-gray-100">Connect with me now!</h2>
+      {/*add arrow down */}
 
-          <p className="text-center dark:text-gray-300 py-6 md:px-12 lg:px-20">
-            I'm currently looking for new opportunities, my inbox is always open. Whether
-            you have a question or just want to say hi, feel free to drop a line.
-          </p>
+      <p className="text-center dark:text-gray-300 py-6">
+        I'm currently looking for new opportunities, my inbox is always open. Whether you
+        have a question or just want to say hi, feel free to drop a line.
+      </p>
 
-          <div className="divider my-1"></div>
-          <div className="flex justify-center items-center gap-20">
-            <Linkedin />
-            <Github />
-            <Gmail />
-          </div>
-        </div>
-      </div>
-      <div
-        id="stack-1"
-        className="card text-center bg-gray-100 dark:bg-gray-700 card bg-base-100 mb-4 border-2 border-gray-300 dark:border-gray-800"
-      >
-        <div className="card-body"></div>
-      </div>
-      <div
-        id="stack-2"
-        className="card text-center bg-gray-200 dark:bg-gray-700 card bg-base-100  border-2 border-gray-300 dark:border-gray-800 shadow-xl"
-      >
-        <div className="card-body"></div>
+      <div className="flex justify-center items-center gap-12 md:gap-18 lg:gap-24 pt-6">
+        <Linkedin />
+        <Github />
+        <Gmail />
       </div>
     </div>
   );
