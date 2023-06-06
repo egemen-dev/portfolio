33909cb8d0c22d132fb87a7637b817c3a5dba6e7
egemen-dev <egemenwasd@gmail.com>
2023-06-06 21:08:42 +0300

    Remove navbarsrc/components/Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Welcome from "./components/Welcome";
 import Skills from "./components/Skills";
@@ -7,6 +6,11 @@ import Footer from "./components/Footer";
 import Stack from "./components/Stack";
 
 export default function App() {
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.bottom * 0.65 <= window.innerHeight;
+  }
+
   function revealCards() {
     const cards = document.querySelectorAll(".card");
     cards.forEach((card) => {
@@ -21,11 +25,6 @@ export default function App() {
           card.classList.add("out");
         }
       }
-
-      function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return rect.bottom * 0.85 <= window.innerHeight;
-      }
     });
   }
 
@@ -33,8 +32,6 @@ export default function App() {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      {/* <Navbar /> */}
-
       <Welcome />
 
       <div className="flex flex-col justify-center items-center gap-10">
@@ -54,54 +51,68 @@ export default function App() {
               "Tailwind CSS",
             ]}
             filename={"weather-app.gif"}
+            code={"https://github.com/egemen-dev/weather_app"}
+            live={"https://egemen-dev.github.io/weather_app/"}
           />
           <Card
             appName="Full Stack Social Media App"
             description="This is a full stack social media app where users can connect with others and can create, comment and like posts."
             stack={["Ruby on Rails", "PostgreSQL", "Hotwire", "Bulma CSS"]}
             filename={"odinbar.gif"}
+            code={"https://github.com/egemen-dev/odinbar"}
           />
           <Card
             appName="Task Manager App"
             description="This is a todo list app where users can create, edit and delete tasks. Tasks can be dragged and dropped to change their priority order. Task are automatically saved to local storage for next session."
             stack={["React", "SortableJS", "HTML", "Tailwind CSS"]}
             filename={"todo.gif"}
+            code={"https://github.com/egemen-dev/react_todo_app"}
+            live={"https://egemen-dev.github.io/react_todo_app/"}
           />
           <Card
             appName="Device Tracker App"
             description="This is a device tracker app. Admins can register devices and monitor their statuses as they are checked in and out."
             stack={["Ruby on Rails", "Simple CSS", "SQLite3"]}
             filename={"active-devices.jpg"}
+            code={"https://github.com/egemen-dev/active_devices"}
           />
           <Card
             appName="Professional CV Builder"
             description="This is a cv builder app. Users can create a professional cv by filling out a dynamic form and then download it as a pdf."
             stack={["React", "HTML", "Tailwind CSS"]}
             filename={"cv-builder.gif"}
+            code={"https://github.com/egemen-dev/react_cv_builder_app"}
+            live={"https://egemen-dev.github.io/react_cv_builder_app/"}
           />
           <Card
             appName="Admin Dashboard"
             description="This is an admin dashboard. It is a responsive dashboard with a sidebar and a navbar. No frameworks were used. Pure HTML and CSS."
             stack={["HTML", "CSS"]}
             filename={"dashboard.gif"}
+            code={"https://github.com/egemen-dev/admin_dashboard"}
+            live={"https://egemen-dev.github.io/admin_dashboard/"}
           />
           <Card
             appName="Flight Booker App"
             description="This is a flight booker app. Users can search for flights and book them. Passengers receive an email confirmation after booking."
             stack={["Ruby on Rails", "Bulma CSS", "PostgreSQL"]}
             filename={"flight-booker.gif"}
+            code={"https://github.com/egemen-dev/flight_booker"}
           />
           <Card
             appName="Etch-a-Sketch"
             description="This is an etch-a-sketch app. Users can draw on the canvas by hovering over it. They can also change the color of the pen and the size of the canvas."
             stack={["HTML", "CSS", "JavaScript"]}
             filename={"colors.gif"}
+            code={"https://github.com/egemen-dev/etch-a-sketch"}
+            live={"https://egemen-dev.github.io/etch-a-sketch/"}
           />
           <Card
             appName="Knights Travails"
             description="This is a knights travails app. It finds the shortest path for a knight to move from one square to another on a chess board."
             stack={["Ruby"]}
             filename={"chess-board.gif"}
+            code={"https://github.com/egemen-dev/knights_travails"}
           />
         </div>
         <Skills />
--- src/components/Stack.js
+++ src/components/Stack.js
@@ -6,17 +6,24 @@ export default function Stack() {
   return (
     <div className="card-body items-center shadow-xl bg-base-100 rounded-box dark:bg-base-200">
       <h2 className="card-title dark:text-gray-100">Connect with me now!</h2>
-      {/*add arrow down */}
 
       <p className="text-center dark:text-gray-300 py-6">
         I'm currently looking for new opportunities, my inbox is always open. Whether you
         have a question or just want to say hi, feel free to drop a line.
       </p>
 
-      <div className="flex justify-center items-center gap-12 md:gap-18 lg:gap-24 pt-6">
-        <Linkedin />
-        <Github />
-        <Gmail />
+      <div className="flex justify-center items-center gap-6 md:gap-8 lg:gap-12 pt-6">
+        <a href="https://www.linkedin.com/in/egemenn/">
+          <Linkedin />
+        </a>
+        <div className="divider border border-gray-200"></div>
+        <a href="https://github.com/egemen-dev">
+          <Github />
+        </a>
+        <div className="divider border border-gray-200"></div>
+        <a href="mailto:egemenwasd@gmail.com">
+          <Gmail />
+        </a>
       </div>
     </div>
   );
