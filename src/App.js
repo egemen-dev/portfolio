162451de2e48679a1f5521caebe288bf7600ae81
egemen-dev <egemenwasd@gmail.com>
2023-06-06 16:44:17 +0300

    Add projects and descriptions with skills

--- src/App.js
+++ src/App.js
@@ -41,16 +41,71 @@ export default function App() {
         <h2 className="text-3xl font-bold text-center mt-24">Projects</h2>
         <div className="divider mb-24"></div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-14">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-16 px-8">
+          <Card
+            appName="Weather Forecast App"
+            description="This is a weather weather forecast app. Users can search for a city from the custom search bar and get the current weather forecast for next 7 days."
+            stack={[
+              "JavaScript",
+              "HTML",
+              "CSS",
+              "Open-Meteo API",
+              "Webpack",
+              "Tailwind CSS",
+            ]}
+            filename={"weather-app.gif"}
+          />
+          <Card
+            appName="Full Stack Social Media App"
+            description="This is a full stack social media app where users can connect with others and can create, comment and like posts."
+            stack={["Ruby on Rails", "PostgreSQL", "Hotwire", "Bulma CSS"]}
+            filename={"odinbar.gif"}
+          />
+          <Card
+            appName="Task Manager App"
+            description="This is a todo list app where users can create, edit and delete tasks. Tasks can be dragged and dropped to change their priority order. Task are automatically saved to local storage for next session."
+            stack={["React", "SortableJS", "HTML", "Tailwind CSS"]}
+            filename={"todo.gif"}
+          />
+          <Card
+            appName="Device Tracker App"
+            description="This is a device tracker app. Admins can register devices and monitor their statuses as they are checked in and out."
+            stack={["Ruby on Rails", "Simple CSS", "SQLite3"]}
+            filename={"active-devices.jpg"}
+          />
+          <Card
+            appName="Professional CV Builder"
+            description="This is a cv builder app. Users can create a professional cv by filling out a dynamic form and then download it as a pdf."
+            stack={["React", "HTML", "Tailwind CSS"]}
+            filename={"cv-builder.gif"}
+          />
+          <Card
+            appName="Admin Dashboard"
+            description="This is an admin dashboard. It is a responsive dashboard with a sidebar and a navbar. No frameworks were used. Pure HTML and CSS."
+            stack={["HTML", "CSS"]}
+            filename={"dashboard.gif"}
+          />
+          <Card
+            appName="Flight Booker App"
+            description="This is a flight booker app. Users can search for flights and book them. Passengers receive an email confirmation after booking."
+            stack={["Ruby on Rails", "Bulma CSS", "PostgreSQL"]}
+            filename={"flight-booker.gif"}
+          />
+          <Card
+            appName="Etch-a-Sketch"
+            description="This is an etch-a-sketch app. Users can draw on the canvas by hovering over it. They can also change the color of the pen and the size of the canvas."
+            stack={["HTML", "CSS", "JavaScript"]}
+            filename={"colors.gif"}
+          />
+          <Card
+            appName="Knights Travails"
+            description="This is a knights travails app. It finds the shortest path for a knight to move from one square to another on a chess board."
+            stack={["Ruby"]}
+            filename={"chess-board.gif"}
+          />
         </div>
         <Skills />
-        <div className="pt-10">
+        <div className="pt-24 flex flex-col justify-center items-center">
           <Stack />
         </div>
       </div>
